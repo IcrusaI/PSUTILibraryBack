84ad5b882106a75a37e8a81c3c2e625b839f2ceb
Icrus <artembazhenov2000@gmail.com>
2022-05-05 12:34:58 +0400

    z

--- data/data.sql
+++ data/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO authors (name) VALUES
-                                   ('asd'),
-                                   ('asd1'),
-                                   ('asd2'),
-                                   ('asd3');
+                                   ('Антипова, Т. А.'),
+                                   ('Николаев, Б. И.');
 
 INSERT INTO books (title, author_id, faculty, semester, download_link) VALUES
-                                                                           ('Test', 1, 'FIST', 1, 'https://example.com');
+                                                                           ('Анализ и прогнозирование поведения временных рядов в физических, биологических и экономических системах', 1, 'FBTO', 3, 'http://elib.psuti.ru/Antipova_Adyshirin-zade_Voronoj_Klyuev_Sokolova_analiz_i_prognozirovanie_povedeniya_vremennyh_ryadov_uchebnik_2021.pdf'),
+                                                                           ('Методы цифровой модуляции сигналов', 2, 'FTR', 2, 'http://elib.psuti.ru/Nikolaev_Chingaeva_Borisenkov_metody_cifrovoj_modulyacii_signalov_lab_rabota_2021.pdf'),
+                                                                           ('Спектры детерминированных сигналов', 2, 'FIST', 5, 'http://elib.psuti.ru/Nikolaev_Chingaeva_Borisenkov_spektry_determinirovannyh_signalov_lab_rabota_2021.pdf');
